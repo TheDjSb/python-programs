199a28b3c4517fa08115115d46f7ddc217c631ea
Subarna Basnet <46018492+TheDjSb@users.noreply.github.com>
2020-08-01 16:05:14 +0545

    Delete basic-arithmetic.py

--- basic-arithmetic.py
+++
@@ -1,8 +0,0 @@
-#WAP to perform all arithmetic operations in python(+,-,*,/,%)
-a=30
-b=19
-print ("sum : ",a+b),
-print ("sub : ",a-b),
-print ("Mul : ",a*b),
-print ("Div : ",a/b),
-print ("Mod : ",a%2)
