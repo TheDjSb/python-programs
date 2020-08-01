0031785ba0af264e0bca41fdcf13c61feda5cac7
Subarna Basnet <46018492+TheDjSb@users.noreply.github.com>
2020-08-01 16:03:03 +0545

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,4 @@
+#Wap to Find area of circle 
+rad=int(input("Enter the Radius of circle : "))
+c=3.14*rad*rad
+print("Area of circle is :",c)
---
+++ basic-arithmetic.py
@@ -0,0 +1,8 @@
+#WAP to perform all arithmetic operations in python(+,-,*,/,%)
+a=30
+b=19
+print ("sum : ",a+b),
+print ("sub : ",a-b),
+print ("Mul : ",a*b),
+print ("Div : ",a/b),
+print ("Mod : ",a%2)
