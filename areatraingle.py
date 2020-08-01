af4727c9a25fb4a9ae142719c482cd3d49751cf9
Subarna Basnet <46018492+TheDjSb@users.noreply.github.com>
2020-08-01 16:04:46 +0545

    Delete areatraingle.py

--- areatraingle.py
+++
@@ -1,7 +0,0 @@
-#WAP to find area of triangle
-a=int(input("First side of triangle:"))
-b=int(input("Second side of triangle:"))
-c=int(input("Third side of triangle:"))
-s=(a+b+c) / 2
-area=(s*(s-a)*(s-b)*(s-c))**0.5
-print("Area of triangle:",area)
